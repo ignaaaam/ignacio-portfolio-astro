70eb1dae3847a9ba28f19a7cd84c472728c2e64a
Ignacio <ignasiamat10@gmail.com>
2023-10-15 21:42:42 +0200

    Reverted back to static. No need to SSR.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-// import vercel from "@astrojs/vercel/static";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
-  output: "server",
+  output: "static",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
