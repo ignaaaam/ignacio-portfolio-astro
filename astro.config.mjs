08b65a4c2546e599c8b5bd2ec513ff4f462f1303
Ignacio <ignasiamat10@gmail.com>
2023-12-31 03:09:43 +0100

    package upgrade. AStro icon upgrade.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import vercel from "@astrojs/vercel/static";
 import vercel from "@astrojs/vercel/serverless";
 import partytown from '@astrojs/partytown';
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
     service: squooshImageService(),
   },
   integrations: [
+    icon(),
     sitemap(),
     partytown({
       config: {
